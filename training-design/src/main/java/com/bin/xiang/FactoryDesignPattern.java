02fb14a5f4ecda1bee94c041295eba8c127de80d
xiangbin <xiangb@ziroom.com>
2018-07-07 17:01:40 +0800

    Initial commit

---
+++ training-design/src/main/java/com/bin/xiang/FactoryDesignPattern.java
@@ -0,0 +1,19 @@
+package com.bin.xiang;
+
+/**
+ * <p></p>
+ * <p>
+ * <PRE>
+ * <BR>    修改记录
+ * <BR>-----------------------------------------------
+ * <BR>    修改日期         修改人          修改内容
+ * </PRE>
+ *
+ * @author xiangbin
+ * @version 1.0
+ * @Date Created in 2018年07月07日 16:54
+ * @since 1.0
+ */
+public class FactoryDesignPattern {
+
+}
